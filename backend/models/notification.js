21b4dfaccc1ee7e449ff12b6d5aede7989f12a4f
Leon <leonardusprabowo14@gmail.com>
2025-10-05 00:07:30 +0700

    notif frontend backend

---
+++ backend/models/notification.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    message: { type: String, required: true },
+    timestamp: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Notification", notificationSchema);
